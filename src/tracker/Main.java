ac3817f0f76bc7ed21d6b570b6da90c4f75771e1
Sam Coco <Samuel.HenriAC@outlook.com>
2019-04-20 23:43:31 +0100

    Code for setting icons removed.

--- src/tracker/Main.java
+++ src/tracker/Main.java
@@ -6,7 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -17,9 +16,10 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("tracker.fxml"));
         primaryStage.setTitle("Spending Tracker");
-        primaryStage.setScene(new Scene(root, 1200, 600));
+        Scene scene = new Scene(root, 1200, 600);
+        primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
     }
-
 }
+
